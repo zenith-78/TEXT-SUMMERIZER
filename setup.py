fdd3b2fca5fb8419ae68c7d0d132fa4598cb9d87
zenith-78 <anshumankhatua78@gmail.com>
2024-06-26 01:53:37 +0530

    requirements and installation done

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding= "utf-8") as f : 
+    long_description = f.read()
+    
+__version__ = "0.0.0"
+
+REPO_NAME = "TEXT-SUMMERIZER"
+AUTHOR_USER_NAME = "zenith-78"
+SRC_REPO = "textSummerizer"
+AUTHOR_EMAIL = "anshumankhatua78@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A text summarizer for NLP Application",
+    long_description=long_description,
+    long_description_content_type="text/markdown", 
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls = {
+        "Bug Tracker" : f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+        },
+    
+    package_dir={"": "src"},
+    packages= setuptools.find_packages(where = "src")
+    
+)
